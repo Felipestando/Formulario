9ac2fb295544b0d4bb71e192451fae0a6e83a365
Felipe Vieira <felipewc@hotmail.com>
2020-10-20 18:31:16 -0300

    inserindo dados

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 var dados, index;
 
+function validacao() {
+	if(document.formularioDados.nome.value == ""){
+		alert("Por Favor, preencha o campo nome.");
+		document.formularioDados.nome.focus();
+		return false;
+	}
+    
+}
+
 function cadPessoa(nome, data, sexo, hobby, estado, msg){
 	dados = document.getElementById("formulario")
 	var qtdLinhas = dados.rows.length;
 	var linha = dados.insertRow(qtdLinhas);
 	var linhaParam;
 
+debugger;
+
 	//validando os hobby
-	var skillLength = document.formdados.hobby.length;
+	var skillLength = document.formularioDados.hobby.length;
 	var hobby = "";
 
 	for(i=0; i<skillLength; i++){
-		var skillChecked = document.formdados.hobby[i].checked;
+		var skillChecked = document.formularioDados.hobby[i].checked;
 		if(skillChecked) {
 
-			hobby += document.formdados.hobby[i].value+", ";
+			hobby += document.formularioDados.hobby[i].value+", ";
 		}
 	}
 	// validando o sexo
-	var sexo = document.formdados.sexo;
+	var sexo = document.formularioDados.sexo;
 
 	for(i=0; i<sexo.length; i++){
 		if(sexo[i].checked){
 			sexo[i].checked = true;
-			sexo = document.formdados.sexo[i].value;
+			sexo = document.formularioDados.sexo[i].value;
 		}
 	}
-	//inserindo os dados nas linhas
+	//inserindo as colunas
 	var cellNome = linha.insertCell(0);
 	var cellData = linha.insertCell(1);
 	var cellSexo = linha.insertCell(2);
@@ -42,26 +53,40 @@ function cadPessoa(nome, data, sexo, hobby, estado, msg){
 	cellEstado.innerHTML = estado;
 	cellMsg.innerHTML = msg;
 
-	preencherAlteração();
+    document.getElementById("formularioDados").reset();
+	preencherAlteracao();
 
 }
 
+     
 function alterDados(nome, data, sexo, hobby, estado, msg){
 	//alterando os dados da tabela
-	dados.rows[index].cells[1].innerHTML = nome;
-	dados.rows[index].cells[2].innerHTML = data;
-	dados.rows[index].cells[3].innerHTML = sexo;
-	dados.rows[index].cells[4].innerHTML = hobby;
-	dados.rows[index].cells[5].innerHTML = estado;
-	dados.rows[index].cells[6].innerHTML = msg;
+	var skillLength = document.formularioDados.hobby.length;
+	var hobby = "";
+	for(i=0; i<skillLength; i++){
+		var skillChecked = document.formularioDados.hobby[i].checked;
+		if(skillChecked) {
+
+			hobby += document.formularioDados.hobby[i].value+", ";
+		}
+	}
+
+	dados.rows[index].cells[0].innerHTML = nome;
+	dados.rows[index].cells[1].innerHTML = data;
+	dados.rows[index].cells[2].innerHTML = sexo;
+	dados.rows[index].cells[3].innerText = hobby;
+	dados.rows[index].cells[4].innerHTML = estado;
+	dados.rows[index].cells[5].innerHTML = msg;
+
 }
 
-function preencherAlteração() {
+function preencherAlteracao() {
 	for(i = 0; i < dados.rows.length; i++){
 
 		dados.rows[i].onclick = function()
 		{
-			debugger;
+			
+		document.getElementById("formularioDados").reset();
 		index = this.rowIndex;
 		document.getElementById("nome").value = dados.rows[index].cells[0].innerText;
 		document.getElementById("data").value = dados.rows[index].cells[1].innerText;
@@ -77,36 +102,43 @@ function preencherAlteração() {
 			{
 			document.getElementById("sexoFeminino").checked = true;
 		}
-
+		debugger;
 		var hobby = dados.rows[index].cells[3].innerText;
-		if(hobby == "Futebol,")
-		{
-			document.getElementById("Futebol").checked = true;
-		}
-		else  if(hobby == "Pescar")
-			{
-			document.getElementById("Pescar").checked = true;
-		}
-		else  if(hobby == "Ler")
-			{
-			document.getElementById("Ler").checked = true;
-		}
-		else  if(hobby == "Academia")
-			{
-			document.getElementById("Acadmeia").checked = true;
-		}
-		else  if(hobby == "Natação")
-			{
-			document.getElementById("Natação").checked = true;
-		}
-		else  if(hobby == "Ciclismo")
-			{
-			document.getElementById("Ciclismo").checked = true;
-		}
+		var lista = hobby.split(",");
+		for(var i=0; i<lista.length; i++){
+			
+			var hobbyAtual = lista[i];
 
+			if(hobbyAtual == "Futebol")
+			{
+				document.getElementById("Futebol").checked = true;
+		    }
+		    else if(hobbyAtual == " Pescar")
+			{
+				document.getElementById("Pescar").checked = true;
+		    }
+		    else if(hobbyAtual == " Ler")
+			{
+				document.getElementById("Ler").checked = true;
+		    }
+		    else if(hobbyAtual == " Academia")
+			{
+				document.getElementById(" Acadmeia").checked = true;
+		    }
+		    else if(hobbyAtual == " Natação")
+			{
+				document.getElementById("Natação").checked = true;
+		    }
+		    else if(hobbyAtual == " Ciclismo")
+			{
+				document.getElementById("Ciclismo").checked = true;
+		    }	
 		}
+		
 	}
 
+  }
+  document.getElementById("formularioDados").reset();
 }
 
 function deletar(){
@@ -116,6 +148,10 @@ function deletar(){
 		if (index == i){
 			dados.deleteRow(index);
 			return;
-		}
 	}
 }
+document.getElementById("formularioDados").reset();
+}
+
+
+
